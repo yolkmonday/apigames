1c57d8debfaef23e9f21c372064876af1e3b8bea
aripadrian <aripadrian01@gmail.com>
2022-09-08 14:30:19 +0700

    add cek saldo

---
+++ index.js
@@ -0,0 +1,39 @@
+const crypto = require('crypto')
+const rp = require('request-promise-native');
+
+class Apigames {
+  /**
+   * @param {string} merchant - Merchant ID
+   * @param {string} secret - Secret Key
+   */
+  constructor(merchant, secret) {
+    this._merchant = merchant;
+    this._secret = secret;
+    this._endpoint = 'https://v1.apigames.id'
+  }
+
+  cekSaldo() {
+    let signature = crypto.createHash('md5').update(`${this._merchant}${this._secret}`).digest('hex')
+    const options = {
+      method: 'GET',
+      uri: `${this._endpoint}/merchant/${this._merchant}?signature=${signature}`,
+      json: true
+    };
+
+    return rp(options)
+      .then(function (resp) {
+        if (resp.data) {
+          if (typeof resp.data.saldo !== undefined) {
+            return resp.data.saldo;
+          } else {
+            throw Error(resp.data.error_msg)
+          }
+        }
+      })
+      .catch(function (err) {
+        throw Error(err)
+      });
+  }
+}
+
+module.exports = Apigames
---
+++ tes.js
@@ -0,0 +1,5 @@
+const Apigames = require('./index')
+
+const apiSdk = new Apigames('M220214LJPD6374UM','30d19bbcd6c9784c020b135c818e8291c00e1a3d12e143c7bb924492c1e57cfb')
+
+apiSdk.cekSaldo()
