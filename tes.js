c589c331f566f8c97c4c9f7586e5515c800bb94c
Ari Padrian <32402214+aripadrian@users.noreply.github.com>
2022-09-29 09:25:10 +0700

    Delete tes.js

--- tes.js
+++
@@ -1,7 +0,0 @@
-const Apigames = require('./index')
-
-const apiSdk = new Apigames('M220214LJPD6374UM', '30d19bbcd6c9784c020b135c818e8291c00e1a3d12e143c7bb924492c1e57cfb')
-
-apiSdk.cekAkunGame('freefire', '1325070844').then(r => {
-  console.log(r);
-})
